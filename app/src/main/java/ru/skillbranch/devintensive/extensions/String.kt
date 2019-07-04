34ba95429f900840f5cabfd670bf0f7323aa3634
dmisuvorov` <dmitrysuvorov9@gmail.com>
2019-07-04 12:44:51 +0300

    add method plural

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,8 +1,10 @@
 package ru.skillbranch.devintensive.extensions
 
 fun String.truncate(countTruncation: Int = 16) : String {
-    val truncateString = this.take(countTruncation).dropLastWhile { it.isWhitespace() }
+    val truncateString = this.take(countTruncation).replace("\\s$".toRegex(), "")
     return if (truncateString.filter { !it.isWhitespace() }.count() == this.filter { !it.isWhitespace() }.count()) truncateString else "$truncateString..."
 }
 
 fun String.stripHtml() = this.replace("<.*?>".toRegex(), "").replace("\\s\\s+".toRegex(), " ")
+
+fun plural(timeUnits: TimeUnits, value: Int) = timeUnits.plural(value)
