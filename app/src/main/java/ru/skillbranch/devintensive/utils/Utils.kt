bc7ffd5f419bfcea1f7baea104241fbc1403d669
dmitry <dmitrysuvorov9@gmail.com>
2019-07-31 22:05:05 +0300

    Fix regex in repo valid

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -4,7 +4,7 @@ package ru.skillbranch.devintensive.utils
 object Utils {
     private val fullNameRegex = "^[a-zа-я'-]+ [a-zа-я,.'-]+.*".toRegex()
     private val nameRegex = "^[a-zа-я'-]+".toRegex()
-    private val repRegex = "^(http(s)?://|www.|http(s)?://www.)?github.com/(?!((enterprise\\b)|(features\\b)|(topics\\b)|(collections\\b)|(trending\\b)|(events\\b)|(marketplace\\b)|(pricing\\b)|(nonprofit\\b)|(customer-stories\\b)|(security\\b)|(login\\b)|(join\\b)))[a-z\\d](?:[a-z\\d]|-(?=[a-z\\d])){0,38}\$\$".toRegex()
+    private val repRegex = "^(http(s)?://|www.|http(s)?://www.)?github.com/(?!((enterprise\\b)|(features\\b)|(topics\\b)|(collections\\b)|(trending\\b)|(events\\b)|(marketplace\\b)|(pricing\\b)|(nonprofit\\b)|(customer-stories\\b)|(security\\b)|(login\\b)|(join\\b)))[A-z\\d](?:[A-z\\d]|-(?=[A-z\\d])){0,38}\$".toRegex(RegexOption.IGNORE_CASE)
 
 
     private val mapTranslit: HashMap<String, String> = hashMapOf(
