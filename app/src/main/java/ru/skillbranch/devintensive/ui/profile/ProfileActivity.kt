8333cb93ae04554dfb4ef4e7244e2916a33717b3
dmisuvorov` <dmitrysuvorov9@gmail.com>
2019-08-02 16:33:29 +0300

    Fix change

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -118,6 +118,7 @@ class ProfileActivity : AppCompatActivity() {
     }
 
     private fun updateRepoStatus(isValidRepo: Boolean) {
+        wrRepository.visibility = View.VISIBLE
         isValidRepository = isValidRepo
         wrRepository.isErrorEnabled = !isValidRepo
         wrRepository.error = if (!isValidRepo) "Невалидный адрес репозитория" else null
